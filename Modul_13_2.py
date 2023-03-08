8aedfe577c2643ca1f9eca1e257aaed9fd5fbb87
Olha Tkachuk <2006olgaolga2006@ukr.net>
2023-03-08 21:46:04 +0100

    @dev@

---
+++ Modul_13_2.py
@@ -0,0 +1,18 @@
+
+from .clean_stations.csv import stations, engine
+
+ins = stations.insert()
+
+ins = stations.insert().values(namber='USC00518838', latitude='21.21')
+
+conn = engine.connect()
+result = conn.execute(ins)
+conn.execute(ins, [
+   {'namber': 'USC00518837', 'latitude': '21.211'},
+   {'namber': 'USC00518839', 'latitude': '21/212'},
+])
+s = ("SELECT * FROM stations LIMIT 5").fetchall()
+result = conn.execute(s)
+
+for row in result:
+   print(row)
